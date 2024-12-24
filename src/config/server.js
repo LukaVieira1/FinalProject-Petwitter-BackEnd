dd69eb471f58d9df0465cd8f58c7b96e04f6c13d
LukaVieira1 <lukavieira14@gmail.com>
2024-12-23 21:41:32 -0300

    remove: vercel config

--- src/config/server.js
+++ src/config/server.js
@@ -16,21 +16,4 @@ app.register(cors, {
 app.register(helmet);
 app.register(routes);
 
-if (require.main === module) {
-  const start = async () => {
-    try {
-      await app.listen(3000, "0.0.0.0");
-      console.log("Servidor rodando na porta 3000");
-    } catch (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-  };
-
-  start();
-}
-
-export default async (req, res) => {
-  await app.ready();
-  app.server.emit("request", req, res);
-};
+export default app;
