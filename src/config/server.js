c3e76cdd188cb4241be7eafc5003d065fcf49df3
LukaVieira1 <lukavieira14@gmail.com>
2024-12-23 21:00:23 -0300

    add: vercel config

--- src/config/server.js
+++ src/config/server.js
@@ -16,4 +16,21 @@ app.register(cors, {
 app.register(helmet);
 app.register(routes);
 
-export default app;
+if (require.main === module) {
+  const start = async () => {
+    try {
+      await app.listen(3000, "0.0.0.0");
+      console.log("Servidor rodando na porta 3000");
+    } catch (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  };
+
+  start();
+}
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
